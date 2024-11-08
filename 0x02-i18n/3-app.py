091a69277fdf0c5296b245bbf728323e34bcf52b
marshall235 <fredie1738@gmail.com>
2024-11-08 14:37:25 +0300

    Changing the code struture

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -18,11 +18,13 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 # Configure the flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
@@ -34,11 +36,13 @@ def get_locale():
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/')
 def index():
     """summary
     """
     return render_template('3-index.html')
 
+
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug = True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
