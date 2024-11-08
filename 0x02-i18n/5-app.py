564f82cb697535fa307b58a157f645c22919ab2d
marshall235 <fredie1738@gmail.com>
2024-11-08 14:52:30 +0300

    Format the code

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """
+Simple Flask app
 """
 
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
+
+
 class Config(object):
     """
     """
@@ -12,11 +15,13 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 # Configure Flask
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
+
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -24,6 +29,7 @@ users = {
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
+
 
 def get_user():
     """
@@ -35,12 +41,14 @@ def get_user():
         return users.get(int(login_id))
     return None
 
+
 @app.before_request
 def before_request():
     """
     """
     user = get_user()
     g.user = user
+
 
 @babel.localeselector
 def get_locale():
@@ -49,14 +57,18 @@ def get_locale():
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         print(locale)
-        return(locale)
+        return locale
 
     return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route("/")
 def index():
     """
+    render the template
     """
     return render_template("5-index.html")
+
 
 if __name__ == "__main__":
     app.run(port="5000", host="0.0.0.0", debug=False)
