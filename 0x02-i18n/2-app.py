de0c316404d722923c9b14292ba2ffc62e5d2dcf
marshall235 <fredie1738@gmail.com>
2024-11-08 14:30:03 +0300

    modifying the code styling

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """
+Create a get_locale function with the
+babel.localeselector decorator. Use
+request.accept_languages to determine
+the best match with our supported languages.
 """
+
 
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -8,32 +13,37 @@ from flask_babel import Babel
 
 class Config(object):
     """
+    Config Class
     """
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 # Configure the Flask app
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
-#!/usr/bin/env python3
+
 
 @babel.localeselector
 def get_locale():
     """
+    Locale function
     """
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route("/")
 def index():
     """
+    render template
     """
 
     return render_template('2-index.html')
 
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug = True)
 
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
